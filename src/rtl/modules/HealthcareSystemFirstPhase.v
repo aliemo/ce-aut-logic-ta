b0335bf03ad1f73e90d4743619389f521a6bba96
Ali <alimpk@outlook.com>
2020-01-11 14:57:24 +0330

    verilog files added to project

--- src/rtl/modules/HealthcareSystemFirstPhase.v
+++ src/rtl/modules/HealthcareSystemFirstPhase.v
@@ -23,14 +23,12 @@ module HealthcareSystemFirstPhase(
         bloodType,
         fdSensorValue,
         fdFactoryValue,
-        bloodSensor,
         factoryBaseTemp,
         tempSensorValue,
         factoryTempCoef,
         presureAbnormality,
         bloodAbnormality,
         fallDetected,
-        glycemicIndex,
         temperatureAbnormality);
 
 input [5:0] pressureData;
@@ -38,15 +36,13 @@ input [3:0] bloodPH;
 input [2:0] bloodType;
 input [7:0] fdSensorValue;
 input [7:0] fdFactoryValue;
-input [7:0] bloodSensor;
 input [7:0] factoryBaseTemp;
-input [3:0] factoryTempCoef;
 input [3:0] tempSensorValue;
+input [3:0] factoryTempCoef;
 
 output presureAbnormality;
 output bloodAbnormality;
 output fallDetected;
-output [3:0] glycemicIndex;
 output temperatureAbnormality;
 
 
@@ -74,8 +70,4 @@ output temperatureAbnormality;
     .temperatureAbnormality(temperatureAbnormality)
     );
 
-  GlycemicIndexCalculator GlycemicIndexCalculator(
-    .bloodSensor  (bloodSensor),
-    .glycemicIndex(glycemicIndex)
-    );
 endmodule
---
+++ src/rtl/modules/NervousShockDetector.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+`default_nettype none
+
+/*
+**********************************************************
+** Logic Design Final Project Fall, 2019 Semester
+** Amirkabir University of Technology (Tehran Polytechnic)
+** Department of Computer Engineering (CEIT-AUT)
+** Logic Circuit Design Laboratory
+** https://ceit.aut.ac.ir
+**********************************************************
+** Student ID: XXXXXXX
+** Student ID: XXXXXXX
+**********************************************************
+** Module Name: NervousShockDetector
+**********************************************************
+** Additional Comments:
+*/
+
+module NervousShockDetector(
+        clock,
+        inputdata,
+	 	nervousAbnormality);
+
+input clock;
+input inputdata;
+output [1:0] nervousAbnormality;
+	
+	// write your code here, please.
+	
+endmodule
